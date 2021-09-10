1dadbc918b650829c25662d293f32a635833ccce
Matt <EmDecay@users.noreply.github.com>
2021-09-09 22:14:40 -0700

    Minor changes to variable naming to make it more accurate

--- solanaPigQuery.py
+++ solanaPigQuery.py
@@ -9,7 +9,7 @@ sig = json.loads(response_sig.text)["result"][0]["signature"]
 
 request_price = {"jsonrpc": "2.0", "id": 1, "method": "getConfirmedTransaction", "params": [sig]}
 response_price= requests.post("https://explorer-api.mainnet-beta.solana.com", json=request_price)
-price_before = json.loads(response_price.text)["result"]["meta"]["preBalances"][0]
-price_after = json.loads(response_price.text)["result"]["meta"]["postBalances"][0]
+balance_before = json.loads(response_price.text)["result"]["meta"]["preBalances"][0]
+balance_after = json.loads(response_price.text)["result"]["meta"]["postBalances"][0]
 
-print (sig + " --- " + str(price_before/1000000000) + " --- " + str(price_after/1000000000))
+print (sig + " --- " + str(balance_before/1000000000) + " --- " + str(balance_after/1000000000))
