a7566392054fa26021a10c191365d19da33a10a1
Matt <EmDecay@users.noreply.github.com>
2021-09-15 10:19:42 -0700

    dbinit is now a method instead of raw code that executes

--- dbinit.py
+++ dbinit.py
@@ -6,11 +6,12 @@ import os, sqlite3
 db = "solsaleswatch.db"
 
 # If database doesn't yet exist, initialize it with the table structure
-if not os.path.isfile(db):
-    print "No database found; initializing \'" + db + "\'..."
-    # Initialize database
-    dbconn = sqlite3.connect(db)
-    dbc = dbconn.cursor()
-    dbc.execute("CREATE TABLE transaction (name TEXT, collection TEXT, description TEXT, image_url TEXT, cost REAL, txid TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)")
-    dbconn.commit()
-    dbconn.close()
+def dbinit():
+    if not os.path.isfile(db):
+        print "No database found; initializing \'" + db + "\'..."
+        # Initialize database
+        dbconn = sqlite3.connect(db)
+        dbc = dbconn.cursor()
+        dbc.execute("CREATE TABLE transaction (name TEXT, collection TEXT, description TEXT, image_url TEXT, cost REAL, txid TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)")
+        dbconn.commit()
+        dbconn.close()
